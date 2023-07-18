fb65e497e65ed71de8ec26bdc841ce5c296bdd3b
Facundo Garcia Brunetti <facundobrunetti99@gmail.com>
2023-07-18 03:53:44 -0300

    add--comment and footer

--- scripts/main.js
+++ scripts/main.js
@@ -3,9 +3,10 @@ const sliderInner=document.querySelector(".slider--inner");
 const cantImg=document.querySelectorAll('.img--slider');
 
 let index =1;
+
 setInterval(function (){
-    let p=index *-100;
-    sliderInner.style.transform="translateX("+p+"%)"
+    let grade=index *-100;
+    sliderInner.style.transform="translateX("+grade+"%)"
     index++;
     if(cantImg.length-1<index){
         index=0;
